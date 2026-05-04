ac5e7e5ebec70ca820413a910c40d738fb6b2091
DmitryK <megusdp@gmail.com>
2026-05-04 05:08:51 +0000

    Guard fan dispatcher hook for non-fan builds

--- src/zigbee/general_commands.c
+++ src/zigbee/general_commands.c
@@ -2,7 +2,9 @@
 #include "consts.h"
 #include "cover_cluster.h"
 #include "cover_switch_cluster.h"
+#ifdef TUYA_MCU_FANLIGHT
 #include "fan_cluster.h"
+#endif
 #include "hal/printf_selector.h"
 #include "poll_control_cluster.h"
 #include "relay_cluster.h"
@@ -20,8 +22,10 @@ static void zigbee_on_attr_change(uint8_t endpoint, uint16_t cluster_id,
         cover_switch_cluster_callback_attr_write_trampoline(endpoint, attribute_id);
     } else if (cluster_id == ZCL_CLUSTER_ON_OFF) {
         relay_cluster_callback_attr_write_trampoline(endpoint, attribute_id);
+#ifdef TUYA_MCU_FANLIGHT
     } else if (cluster_id == ZCL_CLUSTER_FAN_CONTROL) {
         fan_cluster_callback_attr_write_trampoline(endpoint, attribute_id);
+#endif
     } else if (cluster_id == ZCL_CLUSTER_WINDOW_COVERING) {
         cover_cluster_callback_attr_write_trampoline(endpoint, attribute_id);
     }
